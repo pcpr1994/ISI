b72aaacd8bc304a60fa196ee1c49939679f162b4
pcpr1994 <75262703+pcpr1994@users.noreply.github.com>
2022-12-30 15:18:00 +0000

    tabela feita

---
+++ ISIParkAPI.Data/Repositories/Interfaces/ISpecialUsersRepository.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Grupo 4
+ * Trabalho II de ISI
+ * Alunos
+ *  Carlos Pereira nº6498
+ *  Paula Rodrigues nº21133
+ *  Sérgio Gonçalves nº20343
+ *  
+ */
+using ISIParkAPI.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ISIParkAPI.Data.Repositories.Interfaces
+{
+    public interface ISpecialUsersRepository
+    {
+        Task<IEnumerable<SpecialUser>> GetAllSpecialUser();
+        Task<SpecialUser> GetSpecialUserByID(int id);
+        Task<bool> InsertSpecialUser(SpecialUser specialUser);
+        Task<bool> UpdateSpecialUser(SpecialUser specialUser);
+        Task<bool> DeleteSpecialUser(SpecialUser specialUser);
+    }
+}
---
+++ ISIParkAPI/Controllers/SpecialUsersController.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Grupo 4
+ * Trabalho II de ISI
+ * Alunos
+ *  Carlos Pereira nº6498
+ *  Paula Rodrigues nº21133
+ *  Sérgio Gonçalves nº20343
+ *  
+ */
+using ISIParkAPI.Data.Repositories.Interfaces;
+using ISIParkAPI.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ISIParkAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialUsersController : ControllerBase
+    {
+       
+        private readonly ISpecialUsersRepository _specialUsersRepository;
+
+        public SpecialUsersController(ISpecialUsersRepository specialUsersRepository)
+        {
+            _specialUsersRepository = specialUsersRepository;
+        }
+
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> GetAllSpecialUser()
+        {
+            return Ok(await _specialUsersRepository.GetAllSpecialUser());
+        }
+
+        [HttpGet("{numero}")]
+        public async Task<IActionResult> GetSpecialUserByID(int numero)
+        {
+            return Ok(await _specialUsersRepository.GetSpecialUserByID(numero));
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public async Task<IActionResult> InsertSpecialUser([FromBody] SpecialUser specialUser)
+        {
+            if (specialUser == null)
+                return BadRequest();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var inserted = await _specialUsersRepository.InsertSpecialUser(specialUser);
+            return Created("created", inserted);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> UpdateSpecialUser([FromBody] SpecialUser specialUser)
+        {
+            if (specialUser == null)
+                return BadRequest();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _specialUsersRepository.UpdateSpecialUser(specialUser);
+            return NoContent();
+        }
+
+        [HttpDelete("{numero}")]
+        public async Task<IActionResult> DeleteSpecialUser(int numero)
+        {
+            await _specialUsersRepository.DeleteSpecialUser(new SpecialUser { Id = numero });
+
+            return NoContent();
+        }
+
+
+
+    }
+}
